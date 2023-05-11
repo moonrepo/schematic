21c8afd0a89ebb7ede647cffa0dce3ac0b8bceed
Miles Johnson <mileswjohnson@gmail.com>
2023-05-11 14:12:43 -0700

    Add result.

--- crates/config/src/config_loader.rs
+++ crates/config/src/config_loader.rs
@@ -1,20 +1,28 @@
 use crate::config::{Config, PartialConfig};
 use crate::error::ConfigError;
 use crate::source::{Source, SourceFormat};
+use serde::Serialize;
 use std::marker::PhantomData;
 use std::path::PathBuf;
 
+#[derive(Serialize)]
+pub struct ConfigLoadResult<T: Config> {
+    pub config: T,
+    pub format: SourceFormat,
+    pub sources: Vec<Source>,
+}
+
 pub struct ConfigLoader<T: Config> {
     _config: PhantomData<T>,
-    source_format: SourceFormat,
+    format: SourceFormat,
     sources: Vec<Source>,
 }
 
 impl<T: Config> ConfigLoader<T> {
-    pub fn new(source_format: SourceFormat) -> Self {
+    pub fn new(format: SourceFormat) -> Self {
         ConfigLoader {
             _config: PhantomData,
-            source_format,
+            format,
             sources: vec![],
         }
     }
@@ -37,31 +45,27 @@ impl<T: Config> ConfigLoader<T> {
         Ok(self)
     }
 
-    pub async fn load(&self) -> Result<(), ConfigError> {
-        let partial_layers = self.parse_into_layers().await?;
+    pub async fn load(&mut self) -> Result<(), ConfigError> {
+        let (partial_layers, resolved_sources) = self.parse_into_layers().await?;
 
         Ok(())
     }
 
-    async fn parse_into_layers(&self) -> Result<Vec<T::Partial>, ConfigError> {
+    async fn parse_into_layers(&mut self) -> Result<(Vec<T::Partial>, Vec<Source>), ConfigError> {
         let mut layers: Vec<T::Partial> = vec![];
+        let mut sources: Vec<Source> = vec![];
 
         // First layer should be the defaults
         layers.push(T::Partial::default_values());
 
         // Sources would then overrides the defaults in sequence
-        for source in &self.sources {
-            let partial: T::Partial = source.parse(self.source_format).await?;
+        for source in self.sources.drain(0..) {
+            let partial: T::Partial = source.parse(self.format).await?;
+
             layers.push(partial);
+            sources.push(source);
         }
 
-        Ok(layers)
+        Ok((layers, sources))
     }
 }
-
-// fn test() {
-//     let config: SomeConfig = ConfigLoader::new(SourceFormat::Yaml)
-//         .file("foo.yml")?
-//         .url("https://example.com/bar.yml")?
-//         .load()?;
-// }
--- crates/config/src/source.rs
+++ crates/config/src/source.rs
@@ -1,9 +1,10 @@
 use crate::error::ConfigError;
-use serde::de::DeserializeOwned;
+use serde::{de::DeserializeOwned, Serialize};
 use starbase_utils::fs;
 use std::path::PathBuf;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Serialize)]
+#[serde(rename_all = "lowercase")]
 pub enum SourceFormat {
     #[cfg(feature = "yaml")]
     Yaml,
@@ -25,23 +26,25 @@ impl SourceFormat {
     }
 }
 
+#[derive(Serialize)]
+#[serde(tag = "type", rename_all = "lowercase")]
 pub enum Source {
-    Code(String),
-    File(PathBuf),
-    Url(String),
+    Code { code: String },
+    File { path: PathBuf },
+    Url { url: String },
 }
 
 impl Source {
     pub fn code<T: TryInto<String>>(code: T) -> Result<Source, ConfigError> {
         let code: String = code.try_into().map_err(|_| ConfigError::InvalidCode)?;
 
-        Ok(Source::Code(code))
+        Ok(Source::Code { code })
     }
 
     pub fn file<T: TryInto<PathBuf>>(path: T) -> Result<Source, ConfigError> {
         let path: PathBuf = path.try_into().map_err(|_| ConfigError::InvalidFile)?;
 
-        Ok(Source::File(path))
+        Ok(Source::File { path })
     }
 
     pub fn url<T: TryInto<String>>(url: T) -> Result<Source, ConfigError> {
@@ -51,7 +54,7 @@ impl Source {
             return Err(ConfigError::HttpsOnly);
         }
 
-        Ok(Source::Url(url))
+        Ok(Source::Url { url })
     }
 
     pub async fn parse<D>(&self, format: SourceFormat) -> Result<D, ConfigError>
@@ -59,15 +62,15 @@ impl Source {
         D: DeserializeOwned,
     {
         let content = match self {
-            Source::Code(code) => code.to_owned(),
-            Source::File(path) => {
+            Source::Code { code } => code.to_owned(),
+            Source::File { path } => {
                 if path.exists() {
                     fs::read_file(path)?
                 } else {
                     return Err(ConfigError::MissingFile(path.to_path_buf()));
                 }
             }
-            Source::Url(url) => reqwest::get(url).await?.text().await?,
+            Source::Url { url } => reqwest::get(url).await?.text().await?,
         };
 
         format.parse(content)
