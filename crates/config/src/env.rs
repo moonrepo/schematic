ca2bd0d36d75a81244a9cb00840121da67e0d399
Miles Johnson <milesj@users.noreply.github.com>
2023-06-08 13:20:27 -0700

    new: Allow ignoring empty env vars. (#39)

--- crates/config/src/env.rs
+++ crates/config/src/env.rs
@@ -1,45 +1,57 @@
 use crate::errors::ConfigError;
+use crate::internal;
 use std::str::FromStr;
+
+/// Ignore the environment variable if it's empty and fallback to the previous or default value.
+pub fn ignore_empty<T: FromStr>(var: String) -> Result<Option<T>, ConfigError> {
+    let var = var.trim();
+
+    if var.is_empty() {
+        return Ok(None);
+    }
+
+    internal::parse_value(var).map(|v| Some(v))
+}
 
 /// Parse a string into a boolean. Will parse `1`, `true`, `yes`, `on`,
 /// and `enabled` as true, and everything else as false.
-pub fn parse_bool(var: String) -> Result<bool, ConfigError> {
-    match var.to_lowercase().as_str() {
-        "1" | "true" | "yes" | "on" | "enabled" | "enable" => Ok(true),
-        _ => Ok(false),
-    }
+pub fn parse_bool(var: String) -> Result<Option<bool>, ConfigError> {
+    Ok(match var.to_lowercase().as_str() {
+        "1" | "true" | "yes" | "on" | "enabled" | "enable" => Some(true),
+        _ => Some(false),
+    })
 }
 
-fn split<T: FromStr>(var: String, delim: char) -> Result<Vec<T>, ConfigError> {
+fn split<T: FromStr>(var: String, delim: char) -> Result<Option<Vec<T>>, ConfigError> {
     let mut list = vec![];
 
     for s in var.split(delim) {
-        let value: T = s.trim().parse().map_err(|_| {
-            ConfigError::Message(format!("Failed to parse \"{s}\" into the correct type."))
-        })?;
+        let value = s.trim();
 
-        list.push(value);
+        if !value.is_empty() {
+            list.push(internal::parse_value(value)?);
+        }
     }
 
-    Ok(list)
+    Ok(Some(list))
 }
 
 /// Split a variable on each comma (`,`) and parse into a list of values.
-pub fn split_comma<T: FromStr>(var: String) -> Result<Vec<T>, ConfigError> {
+pub fn split_comma<T: FromStr>(var: String) -> Result<Option<Vec<T>>, ConfigError> {
     split(var, ',')
 }
 
 /// Split a variable on each colon (`:`) and parse into a list of values.
-pub fn split_colon<T: FromStr>(var: String) -> Result<Vec<T>, ConfigError> {
+pub fn split_colon<T: FromStr>(var: String) -> Result<Option<Vec<T>>, ConfigError> {
     split(var, ':')
 }
 
 /// Split a variable on each semicolon (`;`) and parse into a list of values.
-pub fn split_semicolon<T: FromStr>(var: String) -> Result<Vec<T>, ConfigError> {
+pub fn split_semicolon<T: FromStr>(var: String) -> Result<Option<Vec<T>>, ConfigError> {
     split(var, ';')
 }
 
 /// Split a variable on each space (` `) and parse into a list of values.
-pub fn split_space<T: FromStr>(var: String) -> Result<Vec<T>, ConfigError> {
+pub fn split_space<T: FromStr>(var: String) -> Result<Option<Vec<T>>, ConfigError> {
     split(var, ' ')
 }
--- crates/config/src/internal.rs
+++ crates/config/src/internal.rs
@@ -4,24 +4,31 @@ use crate::merge::merge_partial;
 use std::{env, str::FromStr};
 
 pub fn default_from_env_var<T: FromStr>(key: &str) -> Result<Option<T>, ConfigError> {
-    parse_from_env_var(key, |var| {
-        var.parse::<T>()
-            .map_err(|_| ConfigError::Message("Failed to parse into the correct type.".into()))
-    })
+    parse_from_env_var(key, |var| parse_value(var).map(|v| Some(v)))
 }
 
 pub fn parse_from_env_var<T>(
     key: &str,
-    parser: impl Fn(String) -> Result<T, ConfigError>,
+    parser: impl Fn(String) -> Result<Option<T>, ConfigError>,
 ) -> Result<Option<T>, ConfigError> {
     if let Ok(var) = env::var(key) {
         let value =
             parser(var).map_err(|e| ConfigError::InvalidEnvVar(key.to_owned(), e.to_string()))?;
 
-        return Ok(Some(value));
+        return Ok(value);
     }
 
     Ok(None)
+}
+
+pub fn parse_value<T: FromStr, V: AsRef<str>>(value: V) -> Result<T, ConfigError> {
+    let value = value.as_ref();
+
+    value.parse::<T>().map_err(|_| {
+        ConfigError::Message(format!(
+            "Failed to parse \"{value}\" into the correct type."
+        ))
+    })
 }
 
 #[allow(clippy::unnecessary_unwrap)]
