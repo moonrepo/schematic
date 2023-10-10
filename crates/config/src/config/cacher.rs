74c4764c4536e8e7a957dc22c617f5df950dd350
Miles Johnson <mileswjohnson@gmail.com>
2023-10-09 22:26:29 -0700

    Polish.

--- crates/config/src/config/cacher.rs
+++ crates/config/src/config/cacher.rs
@@ -4,10 +4,10 @@ use std::collections::HashMap;
 /// A system for reading and writing to a cache for URL based configurations.
 pub trait Cacher {
     /// Read content from the cache store.
-    fn read(&mut self, _url: &str) -> Result<Option<String>, ConfigError>;
+    fn read(&mut self, url: &str) -> Result<Option<String>, ConfigError>;
 
     /// Write the provided content to the cache store.
-    fn write(&mut self, _url: &str, _content: &str) -> Result<(), ConfigError>;
+    fn write(&mut self, url: &str, content: &str) -> Result<(), ConfigError>;
 }
 
 pub type BoxedCacher = Box<dyn Cacher>;
