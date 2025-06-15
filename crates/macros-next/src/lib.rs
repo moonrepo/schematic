7f8136a8bd0805a5af618952790a5ac818a413bf
Miles Johnson <mileswjohnson@gmail.com>
2025-06-15 15:06:39 -0700

    Add args and crate.

---
+++ crates/macros-next/src/lib.rs
@@ -0,0 +1,43 @@
+mod args;
+
+// mod common;
+// mod utils;
+
+#[cfg(feature = "config")]
+mod config;
+#[cfg(feature = "config")]
+mod config_enum;
+#[cfg(feature = "schema")]
+mod schematic;
+
+// use common::Macro;
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{DeriveInput, parse_macro_input};
+
+// #[derive(Config)]
+#[cfg(feature = "config")]
+#[proc_macro_derive(Config, attributes(config, setting))]
+pub fn config(item: TokenStream) -> TokenStream {
+    let input: DeriveInput = parse_macro_input!(item);
+    let output = config::ConfigMacro(Macro::from(&input));
+
+    quote! { #output }.into()
+}
+
+// #[derive(ConfigEnum)]
+#[cfg(feature = "config")]
+#[proc_macro_derive(ConfigEnum, attributes(config, variant))]
+pub fn config_enum(item: TokenStream) -> TokenStream {
+    config_enum::macro_impl(item)
+}
+
+// #[derive(Schematic)]
+#[cfg(feature = "schema")]
+#[proc_macro_derive(Schematic, attributes(schematic, schema))]
+pub fn schematic(item: TokenStream) -> TokenStream {
+    let input: DeriveInput = parse_macro_input!(item);
+    let output = schematic::SchematicMacro(Macro::from(&input));
+
+    quote! { #output }.into()
+}
