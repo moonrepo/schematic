b33818b24e5993a77d8b11ee400d22b3acd8f862
Miles Johnson <mileswjohnson@gmail.com>
2025-06-19 21:54:18 -0700

    Rework layers.

--- crates/core/src/field_value.rs
+++ crates/core/src/field_value.rs
@@ -4,27 +4,32 @@ use quote::ToTokens;
 use syn::{GenericArgument, Ident, PathArguments, PathSegment, Type};
 
 #[derive(Debug, PartialEq)]
-pub enum WrapperType {
+pub enum Layer {
     Arc,
     Box,
     Option,
     Rc,
+    // Collections
+    Map(String),
+    Set(String),
+    Vec(String),
+    Unknown(String),
 }
 
 #[derive(Debug)]
 pub struct FieldValue {
+    pub layers: Vec<Layer>,
     pub nested: bool,
-    pub nested_ident: Option<FieldNestedIdent>,
+    pub nested_ident: Option<Ident>,
     pub ty: Type,
     pub ty_string: String,
-    pub wrappers: Vec<WrapperType>,
 }
 
 impl FieldValue {
     pub fn new(ty: Type, nested_arg: Option<&FieldNestedArg>) -> Self {
         let mut nested = false;
         let mut nested_ident = None;
-        let mut wrappers = vec![];
+        let mut layers = vec![];
         let ty_string = to_type_string(ty.to_token_stream());
 
         // Determine nested state
@@ -35,7 +40,7 @@ impl FieldValue {
                 }
                 FieldNestedArg::Ident(ident) => {
                     nested = true;
-                    nested_ident = Some(FieldNestedIdent::Unknown(ident.to_owned()));
+                    nested_ident = Some(ident.to_owned());
 
                     if !ty_string.contains(&ident.to_string()) {
                         panic!(
@@ -48,7 +53,7 @@ impl FieldValue {
 
         // Extract type information
         if let Some(custom_ident) =
-            extract_type_information(&ty, &mut wrappers, None, nested && nested_ident.is_none())
+            extract_type_information(&ty, &mut layers, nested && nested_ident.is_none())
         {
             nested_ident = Some(custom_ident);
         }
@@ -62,7 +67,7 @@ impl FieldValue {
         let value = Self {
             nested,
             nested_ident,
-            wrappers,
+            layers,
             ty_string,
             ty,
         };
@@ -73,18 +78,17 @@ impl FieldValue {
     }
 
     pub fn is_outer_option_wrapped(&self) -> bool {
-        self.wrappers
+        self.layers
             .first()
-            .is_some_and(|wrapper| *wrapper == WrapperType::Option)
+            .is_some_and(|wrapper| *wrapper == Layer::Option)
     }
 }
 
 fn extract_type_information(
     ty: &Type,
-    wrappers: &mut Vec<WrapperType>,
-    parent_segment: Option<&PathSegment>,
+    layers: &mut Vec<Layer>,
     nested_ident: bool,
-) -> Option<FieldNestedIdent> {
+) -> Option<Ident> {
     // We don't need to traverse other types, just paths
     let Type::Path(ty_path) = ty else {
         return None;
@@ -94,33 +98,20 @@ fn extract_type_information(
     // instead of the full path `std::option::Option`
     let last_segment = ty_path.path.segments.last().unwrap();
 
-    if last_segment.ident == "Option" {
-        wrappers.push(WrapperType::Option);
-    } else if last_segment.ident == "Arc" {
-        wrappers.push(WrapperType::Arc);
-    } else if last_segment.ident == "Box" {
-        wrappers.push(WrapperType::Box);
-    } else if last_segment.ident == "Rc" {
-        wrappers.push(WrapperType::Rc);
-    }
-
     match &last_segment.arguments {
         // We've reached the final segment
         PathArguments::None => {
             if nested_ident {
-                return extract_nested_ident(&last_segment, parent_segment);
+                return Some(last_segment.ident.clone());
             }
         }
 
         // Attempt to drill deeper down
         PathArguments::AngleBracketed(args) => {
+            extract_layer(last_segment, layers);
+
             if let Some(GenericArgument::Type(inner_ty)) = args.args.last() {
-                return extract_type_information(
-                    inner_ty,
-                    wrappers,
-                    Some(last_segment),
-                    nested_ident,
-                );
+                return extract_type_information(inner_ty, layers, nested_ident);
             }
         }
 
@@ -131,44 +122,28 @@ fn extract_type_information(
     None
 }
 
-fn extract_nested_ident(
-    segment: &PathSegment,
-    parent_segment: Option<&PathSegment>,
-) -> Option<FieldNestedIdent> {
-    let ident = segment.ident.to_owned();
+fn extract_layer(last_segment: &PathSegment, layers: &mut Vec<Layer>) {
+    let layer = if last_segment.ident == "Option" {
+        Layer::Option
+    } else if last_segment.ident == "Arc" {
+        Layer::Arc
+    } else if last_segment.ident == "Box" {
+        Layer::Box
+    } else if last_segment.ident == "Rc" {
+        Layer::Rc
+    } else {
+        let ident = last_segment.ident.to_string();
 
-    if let Some(parent) = parent_segment {
-        let parent_id = parent.ident.to_string();
-
-        if parent_id.ends_with("Vec") {
-            return Some(FieldNestedIdent::Vec(ident));
-        } else if parent_id.ends_with("Set") {
-            return Some(FieldNestedIdent::Set(ident));
-        } else if parent_id.ends_with("Map") {
-            return Some(FieldNestedIdent::Map(ident));
+        if ident.ends_with("Vec") {
+            Layer::Vec(ident)
+        } else if ident.ends_with("Set") {
+            Layer::Set(ident)
+        } else if ident.ends_with("Map") {
+            Layer::Map(ident)
         } else {
-            return None;
+            Layer::Unknown(ident)
         }
-    }
+    };
 
-    Some(FieldNestedIdent::Unknown(ident))
-}
-
-#[derive(Debug, PartialEq)]
-pub enum FieldNestedIdent {
-    Unknown(Ident),
-    Map(Ident),
-    Set(Ident),
-    Vec(Ident),
-}
-
-impl FieldNestedIdent {
-    pub fn get_ident(&self) -> &Ident {
-        match self {
-            Self::Unknown(ident) => ident,
-            Self::Map(ident) => ident,
-            Self::Set(ident) => ident,
-            Self::Vec(ident) => ident,
-        }
-    }
+    layers.push(layer);
 }
