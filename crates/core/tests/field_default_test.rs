827ca1d543a4d02de03cad218b7cf8900cbca1b1
Miles Johnson <mileswjohnson@gmail.com>
2025-06-29 11:01:14 -0700

    Test default values.

--- crates/core/tests/field_default_test.rs
+++ crates/core/tests/field_default_test.rs
@@ -2,113 +2,165 @@ mod utils;
 
 use schematic_core::container::Container;
 use starbase_sandbox::{assert_debug_snapshot, assert_snapshot};
+use std::collections::BTreeMap;
 use syn::parse_quote;
 use utils::pretty;
 
 mod field_default {
     use super::*;
 
+    #[test]
+    fn handles_collections() {
+        let container = Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                a: HashMap<String, String>,
+                b: Vec<u8>,
+                c: BTreeSet<bool>,
+                d: CustomVec<usize>,
+                e: UnknownCollection<isize>,
+            }
+        });
+
+        assert_snapshot!(pretty(container.impl_partial_default_values()));
+    }
+
+    #[test]
+    fn handles_layers() {
+        let container = Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                a: Option<String>,
+                b: Arc<u8>,
+                c: Box<bool>,
+                d: Rc<Option<usize>>,
+                e: Arc<Vec<Option<isize>>>,
+            }
+        });
+
+        assert_snapshot!(pretty(container.impl_partial_default_values()));
+    }
+
+    #[test]
+    fn supports_handler_func() {
+        let container = Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                #[setting(default = handler)]
+                a: String,
+            }
+        });
+
+        assert_snapshot!(pretty(container.impl_partial_default_values()));
+    }
+
     mod named_struct {
         use super::*;
 
         #[test]
-        fn supports_bool() {
+        fn supports_types() {
             let container = Container::from(parse_quote! {
                 #[derive(Config)]
                 struct Example {
+                    no_default: bool,
                     #[setting(default = true)]
                     a: bool,
-                }
-            });
-            let field = container.inner.get_fields()[0];
-
-            assert!(field.args.default.is_some());
-            assert_debug_snapshot!(field.args.default.as_ref().unwrap());
-
-            assert_snapshot!(pretty(container.impl_partial_default_values()));
-        }
-
-        #[test]
-        fn supports_number() {
-            let container = Container::from(parse_quote! {
-                #[derive(Config)]
-                struct Example {
                     #[setting(default = 100)]
-                    a: usize,
-                }
-            });
-            let field = container.inner.get_fields()[0];
-
-            assert!(field.args.default.is_some());
-            assert_debug_snapshot!(field.args.default.as_ref().unwrap());
-
-            assert_snapshot!(pretty(container.impl_partial_default_values()));
-        }
-
-        #[test]
-        fn supports_string() {
-            let container = Container::from(parse_quote! {
-                #[derive(Config)]
-                struct Example {
+                    b: usize,
                     #[setting(default = "abc")]
-                    a: String,
-                }
-            });
-            let field = container.inner.get_fields()[0];
-
-            assert!(field.args.default.is_some());
-            assert_debug_snapshot!(field.args.default.as_ref().unwrap());
-
-            assert_snapshot!(pretty(container.impl_partial_default_values()));
-        }
-
-        #[test]
-        fn supports_array() {
-            let container = Container::from(parse_quote! {
-                #[derive(Config)]
-                struct Example {
+                    c: String,
                     #[setting(default = ["a".into(), "b".into(), "c".into()])]
-                    a: [String; 3],
-                }
-            });
-            let field = container.inner.get_fields()[0];
-
-            assert!(field.args.default.is_some());
-            assert_debug_snapshot!(field.args.default.as_ref().unwrap());
-
-            assert_snapshot!(pretty(container.impl_partial_default_values()));
-        }
-
-        #[test]
-        fn supports_vec() {
-            let container = Container::from(parse_quote! {
-                #[derive(Config)]
-                struct Example {
+                    d: [String; 3],
                     #[setting(default = vec!["a", "b", "c"])]
-                    a: Vec<String>,
+                    e: Vec<String>,
+                    #[setting(default = (10, -10, 0))]
+                    f: (usize, isize, u8),
                 }
             });
-            let field = container.inner.get_fields()[0];
-
-            assert!(field.args.default.is_some());
-            assert_debug_snapshot!(field.args.default.as_ref().unwrap());
 
             assert_snapshot!(pretty(container.impl_partial_default_values()));
+
+            for field in container.inner.get_fields() {
+                if field.ident.as_ref().is_some_and(|id| id != "no_default") {
+                    assert!(field.args.default.is_some());
+                }
+            }
+
+            let defaults = container
+                .inner
+                .get_fields()
+                .into_iter()
+                .map(|field| (field.ident.as_ref().unwrap(), field.args.default.as_ref()))
+                .collect::<BTreeMap<_, _>>();
+
+            assert_debug_snapshot!(defaults);
         }
 
         #[test]
-        fn supports_tuple() {
+        fn renders_nothing_if_all_option_wrapped() {
             let container = Container::from(parse_quote! {
                 #[derive(Config)]
                 struct Example {
-                    #[setting(default = (10, -10, 0))]
-                    a: (usize, isize, u8),
+                    a: Option<String>,
+                    b: Option<Vec<u8>>,
                 }
             });
-            let field = container.inner.get_fields()[0];
 
-            assert!(field.args.default.is_some());
-            assert_debug_snapshot!(field.args.default.as_ref().unwrap());
+            assert_snapshot!(pretty(container.impl_partial_default_values()));
+        }
+    }
+
+    mod unnamed_struct {
+        use super::*;
+
+        #[test]
+        fn supports_types() {
+            let container = Container::from(parse_quote! {
+                #[derive(Config)]
+                struct Example(
+                    bool,
+                    #[setting(default = true)]
+                    bool,
+                    #[setting(default = 100)]
+                    usize,
+                    #[setting(default = "abc")]
+                    String,
+                    #[setting(default = ["a".into(), "b".into(), "c".into()])]
+                    [String; 3],
+                    #[setting(default = vec!["a", "b", "c"])]
+                    Vec<String>,
+                    #[setting(default = (10, -10, 0))]
+                    (usize, isize, u8),
+                );
+            });
+
+            assert_snapshot!(pretty(container.impl_partial_default_values()));
+
+            for field in container.inner.get_fields() {
+                if field.index != 0 {
+                    assert!(field.args.default.is_some());
+                }
+            }
+
+            let defaults = container
+                .inner
+                .get_fields()
+                .into_iter()
+                .map(|field| (field.index, field.args.default.as_ref()))
+                .collect::<BTreeMap<_, _>>();
+
+            assert_debug_snapshot!(defaults);
+        }
+
+        #[test]
+        fn renders_nothing_if_all_option_wrapped() {
+            let container = Container::from(parse_quote! {
+                #[derive(Config)]
+                struct Example(
+                    Option<String>,
+                    Option<Vec<u8>>,
+                );
+            });
 
             assert_snapshot!(pretty(container.impl_partial_default_values()));
         }
