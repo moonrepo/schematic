11784e0981b28ddbedcfee6610428be8e553dcdc
Miles Johnson <mileswjohnson@gmail.com>
2025-08-03 09:42:01 -0700

    Rename tests.

---
+++ crates/core/tests/setting_extend_test.rs
@@ -0,0 +1,143 @@
+mod utils;
+
+use schematic_core::container::Container;
+use starbase_sandbox::assert_snapshot;
+use syn::parse_quote;
+use utils::pretty;
+
+mod setting_extend {
+    use super::*;
+
+    #[test]
+    fn can_set_string() {
+        let container = Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                #[setting(extend)]
+                a: String,
+            }
+        });
+        let field = container.inner.get_fields()[0];
+
+        assert!(field.args.extend);
+        assert_snapshot!(pretty(container.impl_partial_extends_from()));
+    }
+
+    #[test]
+    fn can_set_opt_string() {
+        let container = Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                #[setting(extend)]
+                a: Option<String>,
+            }
+        });
+        let field = container.inner.get_fields()[0];
+
+        assert!(field.args.extend);
+        assert_snapshot!(pretty(container.impl_partial_extends_from()));
+    }
+
+    #[test]
+    fn can_set_vec_string() {
+        let container = Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                #[setting(extend)]
+                a: Vec<String>,
+            }
+        });
+        let field = container.inner.get_fields()[0];
+
+        assert!(field.args.extend);
+        assert_snapshot!(pretty(container.impl_partial_extends_from()));
+    }
+
+    #[test]
+    fn can_set_opt_vec_string() {
+        let container = Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                #[setting(extend)]
+                a: Option<Vec<String>>,
+            }
+        });
+        let field = container.inner.get_fields()[0];
+
+        assert!(field.args.extend);
+        assert_snapshot!(pretty(container.impl_partial_extends_from()));
+    }
+
+    #[test]
+    fn can_set_type() {
+        let container = Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                #[setting(extend)]
+                a: ExtendsFrom,
+            }
+        });
+        let field = container.inner.get_fields()[0];
+
+        assert!(field.args.extend);
+        assert_snapshot!(pretty(container.impl_partial_extends_from()));
+    }
+
+    #[test]
+    fn can_set_opt_type() {
+        let container = Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                #[setting(extend)]
+                a: Option<ExtendsFrom>,
+            }
+        });
+        let field = container.inner.get_fields()[0];
+
+        assert!(field.args.extend);
+        assert_snapshot!(pretty(container.impl_partial_extends_from()));
+    }
+
+    #[test]
+    #[should_panic(expected = "Only 1 setting may use `extend`, found: a, b")]
+    fn errors_multiple_extends() {
+        Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                #[setting(extend)]
+                a: String,
+                #[setting(extend)]
+                b: String,
+            }
+        })
+        .impl_partial_extends_from();
+    }
+
+    #[test]
+    #[should_panic(
+        expected = "Only `String`, `Vec<String>`, or `schematic::ExtendsFrom` are supported"
+    )]
+    fn errors_invalid_type() {
+        Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example {
+                #[setting(extend)]
+                a: bool,
+            }
+        })
+        .impl_partial_extends_from();
+    }
+
+    #[test]
+    #[should_panic(expected = "Only named structs can use `extend` settings.")]
+    fn errors_when_used_in_unnamed_struct() {
+        Container::from(parse_quote! {
+            #[derive(Config)]
+            struct Example(
+                #[setting(extend)]
+                String,
+            );
+        })
+        .impl_partial_extends_from();
+    }
+}
---
+++ crates/core/tests/setting_validate_test.rs
@@ -0,0 +1,94 @@
+use schematic_core::container::Container;
+use syn::parse_quote;
+
+mod setting_validate {
+    use super::*;
+
+    mod named_struct {
+        use super::*;
+
+        #[test]
+        fn accepts_func_ref() {
+            let container = Container::from(parse_quote! {
+                #[derive(Config)]
+                struct Example {
+                    #[setting(validate = func_ref)]
+                    a: String,
+                }
+            });
+            let field = container.inner.get_fields()[0];
+
+            assert!(field.args.validate.is_some());
+        }
+
+        #[test]
+        fn accepts_curried_func() {
+            let container = Container::from(parse_quote! {
+                #[derive(Config)]
+                struct Example {
+                    #[setting(validate = func_call())]
+                    a: String,
+                }
+            });
+            let field = container.inner.get_fields()[0];
+
+            assert!(field.args.validate.is_some());
+        }
+
+        #[test]
+        #[should_panic(expected = "UnexpectedType")]
+        fn errors_invalid_type() {
+            Container::from(parse_quote! {
+                #[derive(Config)]
+                struct Example {
+                    #[setting(validate = 123)]
+                    a: String,
+                }
+            });
+        }
+    }
+
+    mod unnamed_struct {
+        use super::*;
+
+        #[test]
+        fn accepts_func_ref() {
+            let container = Container::from(parse_quote! {
+                #[derive(Config)]
+                struct Example(
+                    #[setting(validate = func_ref)]
+                    String,
+                );
+            });
+            let field = container.inner.get_fields()[0];
+
+            assert!(field.args.validate.is_some());
+        }
+
+        #[test]
+        fn accepts_curried_func() {
+            let container = Container::from(parse_quote! {
+                #[derive(Config)]
+                struct Example(
+                    #[setting(validate = func_call())]
+                    String,
+                );
+            });
+            let field = container.inner.get_fields()[0];
+
+            assert!(field.args.validate.is_some());
+        }
+
+        #[test]
+        #[should_panic(expected = "UnexpectedType")]
+        fn errors_invalid_type() {
+            Container::from(parse_quote! {
+                #[derive(Config)]
+                struct Example(
+                    #[setting(validate = 123)]
+                    String,
+                );
+            });
+        }
+    }
+}
