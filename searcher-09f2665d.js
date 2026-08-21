ea874e4f63075e9f7a3e6fefb5e5448482e93519
milesj <milesj@users.noreply.github.com>
2026-08-21 16:32:22 +0000

    deploy: 372fdb411d13c37fc337e75f7912e41ef18cf0c6

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-19aa7bb6.js',
+                path_to_root + 'searchindex-7603683b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
